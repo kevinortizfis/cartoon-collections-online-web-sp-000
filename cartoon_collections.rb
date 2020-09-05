9255ea31af570fb4cc51f6c4ac9ee5322c338179
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-05 20:52:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,11 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elementals)
+  i = 0
+  calls = []
+  while i < elementals.length
+    
 end
 
 def long_planeteer_calls# code an argument here
